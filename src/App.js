e66384a11026aa56f54c5825be96577818330f32
rohshal <learn.rohits@gmail.com>
2024-04-09 12:55:33 -0400

    Initial project commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+
+const App = () => {
+    return (
+    <div class='categories-container'>
+        <div class='Individual-Catogeries'>
+        <div class='Category-body'>
+            <img></img>
+            <h2>Hats</h2>
+            <p>Shop now</p>
+        </div>
+        </div>
+        <div class='Individual-Catogeries'>
+        <div class='Category-body'>
+            <img></img>
+            <h2>Jackets</h2>
+            <p>Shop now</p>
+        </div>
+        </div>
+        <div class='Individual-Catogeries'>
+        <div class='Category-body'>
+            <img></img>
+            <h2>Sneakers</h2>
+            <p>Shop now</p>
+        </div>
+        </div>
+        <div class='Individual-Catogeries'>
+        <div class='Category-body'>
+            <img></img>
+            <h2>Womens Clothing</h2>
+            <p>Shop now</p>
+        </div>
+        </div>
+        <div class='Individual-Catogeries'>
+        <div class='Category-body'>
+            <img></img>
+            <h2>Mens Clothing</h2>
+            <p>Shop now</p>
+        </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
